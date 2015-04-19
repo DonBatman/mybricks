0cc6b6c88a67c0f105d0f933e305efb557043562
DonBatman <serfdon@gmail.com>
2015-04-19 09:33:38 -0700

    Added support for hardenedclay and ores

--- machine.lua
+++ machine.lua
@@ -1,4 +1,4 @@
-local material = {}
+local mater = {}
 local shape = {}
 local make_ok = {}
 local anzahl = {}
@@ -42,6 +42,12 @@ minetest.register_node("mybricks:machine", {
 			{-0.0625, 0.0625, -0.125, 0, 0.25, 0.125}, -- NodeBox8
 		}
 	},
+	selection_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, -0.5, -0.5, 0.5, 0.5, 0.5},
+		}
+	},
 
 	after_place_node = function(pos, placer)
 	local meta = minetest.env:get_meta(pos);
@@ -69,18 +75,29 @@ on_construct = function(pos)
 		"label[7,1.5;Input:]"..
 		"label[7,3.5;Output:]"..
 		"label[0,0;Choose Brick Stye:]"..
-		"label[1.5,1.5;Brick]"..
-		"image_button[1.5,2;1,1;mybricks_mach1.png;brick; ]"..
-		"label[4,1.5;Squares]"..
-		"image_button[4,2;1,1;mybricks_mach2.png;square; ]"..
-		"label[1.5,3;Small Squares]"..
-		"image_button[1.5,3.5;1,1;mybricks_mach3.png;square_sm; ]"..
-		"label[4,3;Tiles]"..
-		"image_button[4,3.5;1,1;mybricks_mach4.png;tile; ]"..
-		"label[1.5,4.5;Split]"..
-		"image_button[1.5,5;1,1;mybricks_mach5.png;split; ]"..
-		"label[4,4.5;V Split]"..
-		"image_button[4,5;1,1;mybricks_mach6.png;vsplit; ]"..
+--		"label[1,0.5;Brick]"..
+		"image_button[1,1;1,1;mybricks_mach1.png;brick; ]"..
+		"image_button[1,2;1,1;mybricks_mach8.png;smbrick; ]"..
+		"image_button[1,3;1,1;mybricks_mach7.png;tbrick; ]"..
+--		"label[2.5,2.5;Squares]"..
+		"image_button[2.5,1;1,1;mybricks_mach4.png;tile; ]"..
+		"image_button[2.5,2;1,1;mybricks_mach2.png;square; ]"..
+		"image_button[2.5,3;1,1;mybricks_mach3.png;square_sm; ]"..
+--		"label[1.5,4.5;Split]"..
+		"image_button[4,1;1,1;mybricks_mach5.png;split; ]"..
+		"image_button[4,2;1,1;mybricks_mach6.png;vsplit; ]"..
+
+--		"label[1,0.5;Brick]"..
+		"image_button[1,4;1,1;mybricks_mach9.png;brickw; ]"..
+		"image_button[1,5;1,1;mybricks_mach10.png;smbrickw; ]"..
+		"image_button[1,6;1,1;mybricks_mach11.png;tbrickw; ]"..
+--		"label[2.5,2.5;Squares]"..
+		"image_button[2.5,4;1,1;mybricks_mach12.png;tilew; ]"..
+		"image_button[2.5,5;1,1;mybricks_mach13.png;squarew; ]"..
+		"image_button[2.5,6;1,1;mybricks_mach14.png;square_smw; ]"..
+--		"label[1.5,4.5;Split]"..
+		"image_button[4,4;1,1;mybricks_mach15.png;splitw; ]"..
+		"image_button[4,5;1,1;mybricks_mach16.png;vsplitw; ]"..
 		"list[current_player;main;1,7;8,4;]")
 	meta:set_string("infotext", "Brick Machine")
 	local inv = meta:get_inventory()
@@ -93,17 +110,45 @@ on_receive_fields = function(pos, formname, fields, sender)
 	local inv = meta:get_inventory()
 
 if fields["brick"] 
+or fields["smbrick"] 
+or fields["tbrick"] 
 or fields["square"]
 or fields["square_sm"]
 or fields["tile"]
 or fields["split"]
 or fields["vsplit"]
+or fields["brickw"] 
+or fields["smbrickw"] 
+or fields["tbrickw"] 
+or fields["squarew"]
+or fields["square_smw"]
+or fields["tilew"]
+or fields["splitw"]
+or fields["vsplitw"]
 then
 
 	if fields["brick"] then
 		make_ok = "0"
 		anzahl = "1"
 		shape = "mybricks:brick_"
+		if inv:is_empty("ingot") then
+			return
+		end
+	end
+
+	if fields["smbrick"] then
+		make_ok = "0"
+		anzahl = "1"
+		shape = "mybricks:smbrick_"
+		if inv:is_empty("ingot") then
+			return
+		end
+	end
+
+	if fields["tbrick"] then
+		make_ok = "0"
+		anzahl = "1"
+		shape = "mybricks:tbrick_"
 		if inv:is_empty("ingot") then
 			return
 		end
@@ -154,275 +199,236 @@ then
 		end
 	end
 
+	if fields["brickw"] then
+		make_ok = "0"
+		anzahl = "1"
+		shape = "mybricks:brickw_"
+		if inv:is_empty("ingot") then
+			return
+		end
+	end
+
+	if fields["smbrickw"] then
+		make_ok = "0"
+		anzahl = "1"
+		shape = "mybricks:smbrickw_"
+		if inv:is_empty("ingot") then
+			return
+		end
+	end
+
+	if fields["tbrickw"] then
+		make_ok = "0"
+		anzahl = "1"
+		shape = "mybricks:tbrickw_"
+		if inv:is_empty("ingot") then
+			return
+		end
+	end
+
+	if fields["squarew"] then
+		make_ok = "0"
+		anzahl = "1"
+		shape = "mybricks:brick_squarew_"
+		if inv:is_empty("ingot") then
+			return
+		end
+	end
+
+	if fields["square_smw"] then
+		make_ok = "0"
+		anzahl = "1"
+		shape = "mybricks:brick_squarew_sm_"
+		if inv:is_empty("ingot") then
+			return
+		end
+	end
+
+	if fields["tilew"] then
+		make_ok = "0"
+		anzahl = "1"
+		shape = "mybricks:brick_tilew_"
+		if inv:is_empty("ingot") then
+			return
+		end
+	end
+
+	if fields["splitw"] then
+		make_ok = "0"
+		anzahl = "1"
+		shape = "mybricks:brick_splitw_"
+		if inv:is_empty("ingot") then
+			return
+		end
+	end
+
+	if fields["vsplitw"] then
+		make_ok = "0"
+		anzahl = "1"
+		shape = "mybricks:brick_vsplitw_"
+		if inv:is_empty("ingot") then
+			return
+		end
+	end
+
 		local ingotstack = inv:get_stack("ingot", 1)
 		local resstack = inv:get_stack("res", 1)
 ----------------------------------------------------------------------------------
 --register nodes
 ----------------------------------------------------------------------------------
-		if ingotstack:get_name()=="default:sandstone" then
-				material = "default_sandstone"
+
+local mat_tab = {
+		{"default:leaves",		"default_leaves"},
+		{"default:jungleleaves",	"default_jungleleaves"},
+		{"default:pine_needles",	"default_pine_needles"},
+		{"default:brick",		"default_brick"},
+--		{"default:bronzeblock",		"default_bronze_block"},
+--		{"default:cactus",		"default_cactus"},
+		{"default:clay",		"default_clay"},
+--		{"default:coalblock",		"default_coal_block"},
+		{"default:cobble",		"default_cobble"},
+--		{"default:copperblock",		"default_copper_block"},
+		{"default:desert_cobble",	"default_desert_cobble"},
+--		{"default:desert_sand",		"default_desert_sand"},
+		{"default:desert_stone",	"default_desert_stone"},
+--		{"default:diamondblock",	"default_diamond_block"},
+--		{"default:dirt",		"default_dirt"},
+--		{"default:glass",		"default_glass"},
+--		{"default:goldblock",		"default_gold_block"},
+--		{"default:gravel",		"default_gravel"},
+--		{"default:ice",			"default_ice"},
+		{"default:jungletree",		"default_jungletree"},
+		{"default:junglewood",		"default_junglewood"},
+--		{"default:lava_source",		"default_lava"},
+--		{"default:mese",		"default_mese"},
+		{"default:mossycobble",		"default_mossycobble"},
+--		{"default:obsidian",		"default_obsidian"},
+--		{"default:obsidian_glass",	"default_obsidian_glass"},
+--		{"default:obsidianbrick",	"default_obsidian_brick"},
+		{"default:pinetree",		"default_pinetree"},
+		{"default:pinewood",		"default_pinewood"},
+--		{"default:sand",		"default_sand"},
+		{"default:sandstone",		"default_sandstone"},
+		{"default:sandstonebrick",	"default_sandstone_brick"},
+--		{"default:snowblock",		"default_snow"},
+--		{"default:steelblock",		"default_steel_block"},
+		{"default:stone",		"default_stone"},
+		{"default:stonebrick",		"default_stone_brick"},
+		{"default:tree",		"default_tree"},
+--		{"default:water_source",	"default_water"},
+		{"default:wood",		"default_wood"},
+		{"farming:straw",		"farming_straw"},
+		{"wool:black",			"wool_black"},
+		{"wool:blue",			"wool_blue"},
+		{"wool:brown",			"wool_brown"},
+		{"wool:cyan",			"wool_cyan"},
+		{"wool:dark_green",		"wool_dark_green"},
+		{"wool:dark_grey",		"wool_dark_grey"},
+		{"wool:green",			"wool_green"},
+		{"wool:grey",			"wool_grey"},
+		{"wool:magenta",		"wool_magenta"},
+		{"wool:orange",			"wool_orange"},
+		{"wool:pink",			"wool_pink"},
+		{"wool:red",			"wool_red"},
+		{"wool:violet",			"wool_violet"},
+		{"wool:white",			"wool_white"},
+		{"wool:yellow",			"wool_yellow"},
+
+--Batmod
+
+		{"bat_blocks:bat_cobble",       "bat_cobble"},
+		{"bat_blocks:bat_cobble_white", "bat_cobble_white"},
+		{"bat_blocks:bat_cobble_tan",   "bat_cobble_tan"},
+		{"bat_blocks:bat_pavers",       "bat_pavers"},
+		{"bat_blocks:bat_block",        "bat_block"},
+		{"bat_blocks:bat_tile",         "bat_tile"},
+		{"bat_blocks:bat_diag",         "bat_diag"},
+		{"bat_blocks:bat_x",            "bat_x_block"},
+		{"bat_blocks:bat_brick",        "bat_brick"},
+		{"bat_blocks:bat_smbrick",      "bat_smbrick"},
+
+--castle
+
+		{"castle:dungeon_stone",        "dungeon_stone"},
+		{"castle:pavement",       	"pavement_brick"},
+		{"castle:rubble",               "rubble"},
+		{"castle:roofslate",            "slate"},
+		{"castle:stonewall",            "stonewall"},
+
+--moreblocks
+
+		{"moreblocks:cactus_brick",        "cactus_brick"},
+		{"moreblocks:cactus_checker",      "cactus_checker"},
+		{"moreblocks:coal_stone_bricks",   "coal_stone_bricks"},
+		{"moreblocks:circle_stone_bricks", "circle_stone_bricks"},
+		{"moreblocks:iron_checker",        "iron_checker"},
+		{"moreblocks:iron_stone_bricks",   "iron_stone_bricks"},
+		{"moreblocks:plankstone",          "plankstone"},
+		{"moreblocks:stone_tile",          "stone_tile"},
+		{"moreblocks:wood_tile_center",    "wood_tile_center"},
+		{"moreblocks:wood_tile_full",      "wood_tile_full"},
+
+--moretrees
+
+		{"moretrees:apple_planks",      "apple_tree"},
+		{"moretrees:beech_planks",      "beech"},
+		{"moretrees:oak_planks",        "oak"},
+		{"moretrees:sequoia_planks",    "sequoia"},
+		{"moretrees:birch_planks",      "birch"},
+		{"moretrees:palm_planks",       "palm"},
+		{"moretrees:spruce_planks",     "spruce"},
+		{"moretrees:willow_planks",     "willow"},
+		{"moretrees:rubber_tree_planks","rubber_tree"},
+		{"moretrees:fir_planks",        "fir"},
+
+--hardened clay
+
+		{"hardenedclay:hardened_clay" ,                 "hardened_clay"},
+		{"hardenedclay:hardened_clay_white" ,    	"hardened_clay_white"},
+		{"hardenedclay:hardened_clay_black" ,    	"hardened_clay_black"},
+		{"hardenedclay:hardened_clay_light_blue" ,      "hardened_clay_light_blue"},
+		{"hardenedclay:hardened_clay_green" ,    	"hardened_clay_green"},
+		{"hardenedclay:hardened_clay_red" ,    		"hardened_clay_red"},
+		{"hardenedclay:hardened_clay_light_grey" ,      "hardened_clay_light_grey"},
+		{"hardenedclay:hardened_clay_violet" ,    	"hardened_clay_violet"},
+		{"hardenedclay:hardened_clay_lime" ,    	"hardened_clay_lime"},
+		{"hardenedclay:hardened_clay_magenta" ,    	"hardened_clay_magenta"},
+		{"hardenedclay:hardened_clay_orange" ,    	"hardened_clay_orange"},
+		{"hardenedclay:hardened_clay_brown" ,    	"hardened_clay_brown"},
+		{"hardenedclay:hardened_clay_blue" ,    	"hardened_clay_blue"},
+		{"hardenedclay:hardened_clay_yellow" ,    	"hardened_clay_yellow"},
+		{"hardenedclay:hardened_clay_pink" ,    	"hardened_clay_pink"},
+		{"hardenedclay:hardened_clay_cyan" ,    	"hardened_clay_cyan"},
+		{"hardenedclay:hardened_clay_grey" ,    	"hardened_clay_grey"},
+--Ores
+
+		{"ores:slate",             "slate"},
+		{"ores:silt",              "silt"},
+		{"ores:shale",             "shale"},
+		{"ores:schist",            "schist"},
+		{"ores:redsandstone",      "redsandstone"},
+		{"ores:gneiss",            "gneiss"},
+		{"ores:basalt",            "basalt"},
+		{"ores:granite",           "granite"},
+		{"ores:marble",            "marble"},
+
+	}
+for i in ipairs (mat_tab) do
+	local titem = mat_tab[i][1]
+	local tmat  = mat_tab[i][2]
+
+		if ingotstack:get_name()==titem then
+				mater = tmat
 				make_ok = "1"
 		end
 
-		if ingotstack:get_name()=="default:desert_sand" then
-				material = "default_desert_sand"
-				make_ok = "1"
-		end
 
-		if ingotstack:get_name()=="default:clay" then
-				material = "default_clay"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="wool:white" then
-				material = "millwork_white"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="default:desert_stone" then
-				material = "default_desert_stone"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="default:cobble" then
-				material = "default_cobble"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="default:stone" then
-				material = "default_stone"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="default:cactus" then
-				material = "default_cactus"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="wool:white" then
-				material = "millwork_white"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="default:sand" then
-				material = "default_sand"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="default:wood" then
-				material = "default_wood"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="default:pinewood" then
-				material = "default_pinewood"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="default:dirt" then
-				material = "default_dirt"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="default:brick" then
-				material = "default_brick"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="default:bronzeblock" then
-				material = "default_bronze_block"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="default:coalblock" then
-				material = "default_coal_block"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="default:copperblock" then
-				material = "default_copper_block"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="default:desert_cobble" then
-				material = "default_desert_cobble"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="default:diamondblock" then
-				material = "default_diamond_block"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="default:glass" then
-				material = "default_glass"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="default:goldblock" then
-				material = "default_gold_block"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="default:gravel" then
-				material = "default_gravel"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="default:ice" then
-				material = "default_ice"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="default:jungletree" then
-				material = "default_jungletree"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="default:junglewood" then
-				material = "default_junglewood"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="default:lava_source" then
-				material = "default_lava"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="default:mese" then
-				material = "default_mese"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="default:mossycobble" then
-				material = "default_mossycobble"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="default:obsidian" then
-				material = "default_obsidian"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="default:obsidian_glass" then
-				material = "default_obsidian_glass"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="default:obsidianbrick" then
-				material = "default_obsidian_brick"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="default:pinetree" then
-				material = "default_pinetree"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="default:sanddstonebrick" then
-				material = "default_sandstone_brick"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="default:snowblock" then
-				material = "default_snow"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="default:steelblock" then
-				material = "default_steel_block"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="default:stonebrick" then
-				material = "default_stone_brick"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="default:tree" then
-				material = "default_tree"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="default:water_source" then
-				material = "default_water"
-				make_ok = "1"
-		end
-
-		if ingotstack:get_name()=="farming:straw" then
-				material = "farming_straw"
-				make_ok = "1"
-		end
-
-----------------------------------------------------------------------------
---wool
-
-		if ingotstack:get_name()=="wool:white" then
-				material = "wool_white"
-				make_ok = "1"
-		end
-		if ingotstack:get_name()=="wool:black" then
-				material = "wool_black"
-				make_ok = "1"
-		end
-		if ingotstack:get_name()=="wool:blue" then
-				material = "wool_blue"
-				make_ok = "1"
-		end
-		if ingotstack:get_name()=="wool:brown" then
-				material = "wool_brown"
-				make_ok = "1"
-		end
-		if ingotstack:get_name()=="wool:cyan" then
-				material = "wool_cyan"
-				make_ok = "1"
-		end
-		if ingotstack:get_name()=="wool:dark_green" then
-				material = "wool_dark_green"
-				make_ok = "1"
-		end
-		if ingotstack:get_name()=="wool:dark_grey" then
-				material = "wool_dark_grey"
-				make_ok = "1"
-		end
-		if ingotstack:get_name()=="wool:green" then
-				material = "wool_green"
-				make_ok = "1"
-		end
-		if ingotstack:get_name()=="wool:grey" then
-				material = "wool_grey"
-				make_ok = "1"
-		end
-		if ingotstack:get_name()=="wool:magenta" then
-				material = "wool_magenta"
-				make_ok = "1"
-		end
-		if ingotstack:get_name()=="wool:orange" then
-				material = "wool_orange"
-				make_ok = "1"
-		end
-		if ingotstack:get_name()=="wool:pink" then
-				material = "wool_pink"
-				make_ok = "1"
-		end
-		if ingotstack:get_name()=="wool:red" then
-				material = "wool_red"
-				make_ok = "1"
-		end
-		if ingotstack:get_name()=="wool:violet" then
-				material = "wool_violet"
-				make_ok = "1"
-		end
-		if ingotstack:get_name()=="wool:yellow" then
-				material = "wool_yellow"
-				make_ok = "1"
-		end
-
+end
 ----------------------------------------------------------------------
 		if make_ok == "1" then
 			local give = {}
 			for i = 0, anzahl-1 do
-				give[i+1]=inv:add_item("res",shape..material)
+				give[i+1]=inv:add_item("res",shape..mater)
 			end
 			ingotstack:take_item()
 			inv:set_stack("ingot",1,ingotstack)
