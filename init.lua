7bf8ea360e1a9acf056740df39d60dfb1438d629
Don <serfdon@gmail.com>
2025-04-05 06:44:00 -0700

    Add files via upload

--- init.lua
+++ init.lua
@@ -4,12 +4,8 @@ dofile(minetest.get_modpath("mybricks").."/bricks.lua")
 dofile(minetest.get_modpath("mybricks").."/machine.lua")
 dofile(minetest.get_modpath("mybricks").."/register.lua")
 
-if minetest.get_modpath("hardenedclay") then
-	dofile(minetest.get_modpath("mybricks").."/hardenedclay.lua")
-end
-
-if minetest.get_modpath("ores") then
-	dofile(minetest.get_modpath("mybricks").."/ores.lua")
+if minetest.get_modpath("myores") then
+	dofile(minetest.get_modpath("mybricks").."/myores.lua")
 end
 
 
