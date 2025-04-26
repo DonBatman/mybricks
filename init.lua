110170fb0a5ae02a861cc5580c4a3b39cdae2af5
DonBatman <serfdon@gmail.com>
2025-04-26 14:18:35 -0700

    Added mywhiteblock

--- init.lua
+++ init.lua
@@ -7,7 +7,9 @@ dofile(minetest.get_modpath("mybricks").."/register.lua")
 if minetest.get_modpath("myores") then
 	dofile(minetest.get_modpath("mybricks").."/myores.lua")
 end
-
+if minetest.get_modpath("mywhiteblock") then
+	dofile(minetest.get_modpath("mybricks").."/mywhiteblock.lua")
+end
 
 
 
